0b267f0a686b0311c2ad381ab998c09ba9c27211
fahid2002 <fahid23105101323.diu.edu.bd>
2026-03-09 02:26:19 +0600

    3

---
+++ script.js
@@ -0,0 +1,15 @@
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if(username === "admin" && password === "admin123") {
+
+    localStorage.setItem("loggedIn", "true");
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid credentials!");
+  }
+});
