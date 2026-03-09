8cd2af8abff1cd83ee1c88c93da944feb4c8862b
fahid2002 <fahid23105101323.diu.edu.bd>
2026-03-09 16:16:23 +0600

    8

--- dashboard.js
+++ dashboard.js
@@ -56,4 +56,28 @@ function renderIssues() {
   open: 'B13-A5-Github-Issue-Tracker-main/assets/Open-Status.png',
   closed: 'B13-A5-Github-Issue-Tracker-main/assets/Closed- Status .png'
 };
-  })}
+
+
+card.innerHTML = `
+
+  <div class="flex justify-between items-center mb-2">
+
+    <img src="${statusIcons[issue.status]}" 
+         alt="${issue.status}" class="w-5 h-5">
+
+    <span class="text-white text-xs px-2 py-1 rounded-full ${priorityColors[issue.priority.toLowerCase()] || 'bg-gray-400'}">
+      ${issue.priority.toUpperCase()}
+    </span>
+  </div>
+
+  <h2 class="font-bold text-lg text-black mb-1">${issue.title}</h2>
+
+  <p class="text-gray-700 mb-2">${issue.description}</p>
+
+  <p class="text-sm text-gray-500">Author: ${issue.author} | Assignee: ${issue.assignee || 'Unassigned'}</p>
+`;
+
+    card.addEventListener('click', () => showModal(issue));
+    issuesContainer.appendChild(card);
+  });
+}
